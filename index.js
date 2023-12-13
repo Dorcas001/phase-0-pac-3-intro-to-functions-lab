e6c488a3fac0583c08cd7f9ad49d8dc62607e331
karimi <karimidorcas1@gmail.com>
2023-12-13 22:51:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string){
+    return string.toUpperCase()
+}
+shout('hello')
+
+
+function whisper(string){
+    return string.toLowerCase();
+
+}
+
+
+function logShout(string){
+    console.log(string.toUpperCase)
+
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+    
+    }
+
+
+    // function sayHiToHeadphonedRoommate(string){
+    //     if (string == lowercase){
+    //         return "I can't hear you!";
+    //     } else if (string == uppercase){
+    //         return "YES INDEED!";
+    //     }else (string == "Let\'s have dinner together!"){
+    //         return "I would love to!"
+    //     }
+        
+    // }
+    sayHiToHeadphonedRoommate('hello')
+    function sayHiToHeadphonedRoommate(string) {
+        if (string === 'hello')
+        return "I can't hear you!"
+            else if (string === "HELLO")
+        return "YES INDEED!"
+            else (string === "Let\'s have dinner together!")
+            return "I would love to!"
+    }
+    
+
